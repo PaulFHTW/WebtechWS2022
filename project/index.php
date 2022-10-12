ca000d61731e6125cc4eec1a0be5ef2996be14f0
PaulFHTW <paul.bogza@gmail.com>
2022-10-12 07:49:47 +0200

    modified:   project/index.php 	modified:   project/navigation/faq.html 	modified:   project/navigation/impressum.html 	modified:   project/style/index.css 	modified:   project/style/login.css 	modified:   project/user/login.php 	modified:   project/user/register.php

--- project/index.php
+++ project/index.php
@@ -11,13 +11,13 @@
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
-                <a class="navbar-brand" href="index.php">Habbo Hotel</a>
+                    <a class="navbar-brand" href="index.php">Habbo Hotel</a>
                 </div>
                 <ul class="nav navbar-nav">
-                <li><a href="navigation/impressum.html">Impressum</a>
-                <li><a href="navigation/faq.html">FAQ</a>
-                <li><a href="user/register.php">Register</a>
-                <li><a href="user/login.php">Login</a>
+                    <li><a href="navigation/impressum.html">Impressum</a>
+                    <li><a href="navigation/faq.html">FAQ</a>
+                    <li><a href="user/register.php">Register</a>
+                    <li><a href="user/login.php">Login</a>
                 </ul>
             </div>
         </nav>
