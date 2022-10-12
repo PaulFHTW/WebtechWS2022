4f570b02c2259a79f6661bf81e97f860604a44d9
PaulFHTW <paul.bogza@gmail.com>
2022-10-12 07:26:22 +0200

    modified:   project/index.php 	modified:   project/style/index.css 	modified:   project/style/login.css 	modified:   project/user/login.php 	modified:   project/user/register.php

--- project/index.php
+++ project/index.php
@@ -8,11 +8,18 @@
     </head>
     <body>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-        <nav>
-            <li><a href="navigation/impressum.html">Impressum</a>
-            <li><a href="navigation/faq.html">FAQ</a>
-            <li><a href="user/register.php">Register</a>
-            <li><a href="user/login.php">Login</a>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Habbo Hotel</a>
+                </div>
+                <ul class="nav navbar-nav">
+                <li><a href="navigation/impressum.html">Impressum</a>
+                <li><a href="navigation/faq.html">FAQ</a>
+                <li><a href="user/register.php">Register</a>
+                <li><a href="user/login.php">Login</a>
+                </ul>
+            </div>
         </nav>
         <div class="container">
             <img src="images/hotel.jpg" alt="beach-resort">
--- project/user/login.php
+++ project/user/login.php
@@ -4,14 +4,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login</title>
         <link rel="stylesheet" type="text/css" href="../style/login.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" />
     </head>
     <body> 
-        <nav>
-            <li><a href="../index.php">Home</a>
-            <li><a href="../navigation/impressum.html">Impressum</a>
-            <li><a href="../navigation/faq.html">FAQ</a>
-            <li><a href="register.php">Register</a>
-            <li><a href="login.php">Login</a>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                <a class="navbar-brand" href="../index.php">Habbo Hotel</a>
+                </div>
+                <ul class="nav navbar-nav">
+                <li><a href="../index.php">Home</a>
+                <li><a href="../navigation/impressum.html">Impressum</a>
+                <li><a href="../navigation/faq.html">FAQ</a>
+                <li><a href="register.php">Register</a>
+                <li><a href="login.php">Login</a>
+                </ul>
+            </div>
         </nav>
         <form action="../index.php" method="post">
             <p>Log In</p>
