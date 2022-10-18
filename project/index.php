5ceaa22166524e64e76a127dae68a620e9e7b709
PaulFHTW <paul.bogza@gmail.com>
2022-10-18 08:28:21 +0200

    modified:   project/index.php 	modified:   project/navigation/faq.html 	modified:   project/navigation/impressum.html 	modified:   project/user/login.php 	modified:   project/user/register.php

--- project/index.php
+++ project/index.php
@@ -3,22 +3,35 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Index</title>
-        <link rel="stylesheet" type="text/css" href="style/index.css" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">        
+        <link rel="stylesheet" type="text/css" href="index.css" />
     </head>
     <body>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <ul class="nav navbar-nav">
-                    <li><a href="index.php">Home</a>
-                    <li><a href="navigation/impressum.html">Impressum</a>
-                    <li><a href="navigation/faq.html">FAQ</a>
-                    <li><a href="user/register.php">Register</a>
-                    <li><a href="user/login.php">Login</a>
-                </ul>
-            </div>
-        </nav>
-        <div class="container">
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Habbo Hotel</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="user/login.php">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="user/register.php">Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="navigation/faq.html">FAQ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="navigation/impressum.html">Impressum</a>
+            </li>
+        </ul>
+        </div>
+        </div>
+    </nav>
+        <div class="container-fluid">
             <img src="images/hotel.jpg" alt="beach-resort">
         </div>
         <p class="headline">Planen Sie Ihren Traumurlaub</p>
@@ -40,6 +53,5 @@
             Quis viverra nibh cras pulvinar mattis nunc sed blandit libero. Mauris ultrices eros in cursus turpis.
             Nullam ac tortor vitae purus. Bibendum ut tristique et egestas.
         </p>  
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>    </body>
 </html>
