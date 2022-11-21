f4701624a06afac071afe4e2aa2229a135c1da3d
PaulFHTW <paul.bogza@gmail.com>
2022-11-21 15:37:34 +0100

    modified:   .vscode/settings.json 	modified:   README.md 	modified:   project/admin.php 	modified:   project/faq.php 	modified:   project/images/hotel.jpg 	modified:   project/images/logo.png 	modified:   project/images/room1.jpg 	modified:   project/images/room2.jpg 	modified:   project/images/room3.jpg 	modified:   project/impressum.php 	modified:   project/index.php 	modified:   project/login.php 	modified:   project/logout.php 	modified:   project/navigation/navbar.php 	modified:   project/register.php 	modified:   project/reservierung.php 	modified:   project/style/index.css 	modified:   project/style/login.css 	modified:   project/style/room.css

--- project/admin.php
+++ project/admin.php
@@ -4,7 +4,7 @@
 <html lang="de">
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Index</title>
+        <title>Admin</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">        
         <link rel="stylesheet" type="text/css" href="style/index.css" />
     </head>
@@ -20,9 +20,9 @@
 
 </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</html>
 
     <?php
-
         if(isset($_POST["submit"])){
             $file = $_FILES["file"];
             var_dump($_FILES);
@@ -41,7 +41,7 @@
                 if($fileError === 0){
                     if($fileSize < 15000000){
                         $fileNameNew = uniqid('', true).".".$fileActualExt;
-                        $fileDestination = "uploads/".$fileNameNew;
+                        $fileDestination = '../uploads/'.$fileNameNew;
                         move_uploaded_file($fileTmpName, $$fileDestination);
                         echo '<span style=color:green>File uploaded successfully</span>';
                     }else{
@@ -55,5 +55,3 @@
             }
         }
     ?>
-
-</html>
